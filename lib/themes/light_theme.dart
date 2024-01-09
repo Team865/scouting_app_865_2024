8b5e0d032e81aba39c120e0c88d8541e7c960705
Kurupeco <119636806+Kurupeco@users.noreply.github.com>
2024-01-09 18:46:31 -0500

    Update theme

--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-ThemeData calculatorDark = ThemeData(
-    colorScheme: ColorScheme.dark(
+ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.light(
   background: Color.fromARGB(255, 249, 247, 247),
   primary: Color.fromARGB(255, 28, 45, 74),
   secondary: Color.fromARGB(255, 63, 114, 175),
