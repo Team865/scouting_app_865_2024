61134cbb280c6368549aebe5733cbc7aa89a9f13
squareroot-of-false <christopher.whitby@student.tdsb.on.ca>
2024-02-29 16:31:30 -0500

    Fixes some errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:scouting_app_865_2024/themes/light_theme.dart';
+import 'package:scouting_app_865_2024/state.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {  
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: NavigationBar(
-          //insert destinations here
-          destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.videogame_asset_off), label: 'Auto'),
-          NavigationDestination(icon: Icon(Icons.videogame_asset),label: 'Teleop'),
-          NavigationDestination(icon: Icon(Icons.access_time), label: 'Endgame'),
-          NavigationDestination(icon: Icon(Icons.qr_code), label: 'Submission')
-        ]),
-      ),
+      theme: lightTheme,
+      home: const ScoutingApp()
     );
   }
+}
+
+class ScoutingApp extends StatefulWidget {
+  const ScoutingApp({super.key});
+
+  @override
+  State<ScoutingApp> createState() => ScoutingAppState();
 }
--- lib/pages/auto.dart
+++ lib/pages/auto.dart
@@ -60,6 +60,8 @@ class _AutoPageState extends State<AutoPage> {
         ),
         const SizedBox(height:8),
         //ground intake grid (everything below this point)
+        const Text('Ground Intake'),
+        const SizedBox(height:8),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
